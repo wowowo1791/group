fa7baa973543caa4ec56c70788ada451d56c5358
Karyn-Black <caringkaryn5@gmail.com>
2025-03-05 15:30:52 -0500

    Updated console information and added correctly formatted quotation marks

--- homework-5/js/foodJavaScript.js
+++ homework-5/js/foodJavaScript.js
@@ -1,11 +1,11 @@
 // JavaScript Document
 
 const pi = 3.14;
-console.log (pi); // Output: 3.14
+console.log(pi); // Output: 3.14
 
-const food1 = Hamburger;
-const food2 = Cheeseburger;
-const food3 = Fries;
-const food4 = Onion Rings;
+const food1 = "Hamburger";
+const food2 = "Cheeseburger";
+const food3 = "Fries";
+const food4 = "Onion Rings";
 
-console.log (“List of food for sale: “ food1, food2, food3, food4);
+console.log("List of food for sale: ", food1, food2, food3, food4);
