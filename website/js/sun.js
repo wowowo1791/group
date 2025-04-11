bac10716b38baebe45f89221f99988e00663bab0
Karyn-Black <caringkaryn5@gmail.com>
2025-04-11 18:24:09 -0400

    Updated CSS - links now use "cursor: pointer"

--- website/js/sun.js
+++ website/js/sun.js
@@ -1,5 +1,9 @@
 // JavaScript Document
 
+function changeName() {
+  onclick = 'document.getElementById("name").innerHTML = "Sol"';
+}
+
 if (A > 10) {
   document.body.style.background =
     "#f3f3f3 url('img_tree.png') no-repeat right top";
