9c6444a7bdbf519ffab7268a222cf7f8a36d5bab
wowowo1791 <arroyoa@iu.edu>
2025-04-30 14:17:32 -0400

    Implement localStorage

--- website/js/jupiter.js
+++ website/js/jupiter.js
@@ -1,9 +1,14 @@
 const jupiter = document.getElementById("JUPITER");
+let totalClicks = 0;
+// let expSwitch = 0;
 jupiter.onmouseover = beBiggerJupiter;
 jupiter.onmouseout = original;
 
 jupiter.onmousedown = original;
 jupiter.onmouseup = beBiggerJupiter;
+
+jupiter.addEventListener("click", clickCounter);
+// XXXX.addEventListener("click", deleteJupiter);
 
 function beBiggerJupiter() {
     jupiter.style.width = "540px";
@@ -15,3 +20,26 @@ function original() {
     jupiter.style.height = "520px";
     document.getElementById("jupiter-block").style.margin = "0px 0px 150px 0px";
 }
+function clickCounter() {
+    totalClicks += 1;
+    localStorage.setItem("timesClickedOnJupiter", JSON.stringify(totalClicks));
+    /*
+    if (totalClicks => 60) {
+        if (expSwitch = 0) {
+            // change image to explosion
+            // play explosion sound
+        }
+        expSwitch = 1;
+    }
+    */
+}
+/*
+function deleteJupiter() {
+    jupiter.style.display = "none";
+    // play a pop sound here
+    // wait three seconds here
+    localStorage.removeItem("timesClickedOnJupiter");
+    expSwitch = 0;
+    jupiter.style.display = "block";
+}
+*/
--- website/js/saturn.js
+++ website/js/saturn.js
@@ -1,9 +1,14 @@
 const saturn = document.getElementById("SATURN");
+let totalClicks = 0;
+// let expSwitch = 0;
 saturn.onmouseover = meh;
 saturn.onmouseout = original;
 
 saturn.onmousedown = original;
 saturn.onmouseup = meh;
+
+saturn.addEventListener("click", clickCounter);
+// XXXX.addEventListener("click", deleteSaturn);
 
 function meh() {
     saturn.style.width = "1220px";
@@ -15,3 +20,26 @@ function original() {
     saturn.style.height = "675px";
     document.getElementById("saturn-block").style.margin = "0px 0px 150px 0px";
 }
+function clickCounter() {
+    totalClicks += 1;
+    localStorage.setItem("timesClickedOnSaturn", JSON.stringify(totalClicks));
+    /*
+    if (totalClicks => 50) {
+        if (expSwitch = 0) {
+            // change image to explosion
+            // play explosion sound
+        }
+        expSwitch = 1;
+    }
+    */
+}
+/*
+function deleteSaturn() {
+    saturn.style.display = "none";
+    // play a pop sound here
+    // wait three seconds here
+    localStorage.removeItem("timesClickedOnSaturn");
+    expSwitch = 0;
+    saturn.style.display = "block";
+}
+*/
